41ac4034908759582042b7f50a74fba8c08cdb3d
ahlawatshivam <50425905+ahlawatshivam@users.noreply.github.com>
2020-04-18 17:59:00 -0400

    Add files via upload

---
+++ Shivam Ahlawat/post1.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongo = require('mongodb');
+
+var app = express();
+app.use(express.json());
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+app.post('/api/insertMileage/hr', (req, res) => {
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("CarRental");
+	  var myobj={Make: Honda_Sedan,Colour: Yellow,Capacity:4};
+	  dbo.collection("Car").insertOne(myobj, function(err, result) {
+		if (err) throw err;
+		
+		console.log("1 document inserted");
+		res.send(result);
+		db.close();
+	  });
+	});
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
